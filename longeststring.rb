d3b0bcaa9d83f2e2b310590fa40aab891c174dbf
Anthematics <jasonharder2@gmail.com>
2017-11-23 13:19:35 -0500

    basic algorithm worked out for 1 size of array now working so it applies to multiple sizes

---
+++ longeststring.rb
@@ -0,0 +1,30 @@
+
+
+def longeststring(strarr,k)
+	longest = '' #longest = an empty string that will change as we iterate
+	n = strarr.length - (k - 1) #
+
+	n.times do |index|
+
+		fulllength = 0 #full length must change as we iterate
+
+		k.times do |j|
+				strarr[index + j]
+
+		end
+
+
+
+		if longest.length < fulllength #if longest.length is greater than full length then longest now = its place in the array + its place in the array 1 & 2 spaces after it
+			 longest = strarr[index] + strarr[index+1] + strarr[index+2]
+		end
+	end
+	return longest
+end
+
+strarr = ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"]
+
+
+puts longeststring(strarr,k)
+
+#go through my notes.
